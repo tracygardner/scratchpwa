5b959d4100eee459611426c9c909d47142fd51c2
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2022-10-22 19:43:17 +0000

    workbox import

--- scratch-gui/src/src-sw.js
+++ scratch-gui/src/src-sw.js
@@ -2,4 +2,4 @@ import {precacheAndRoute} from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+precacheAndRoute(self.__precacheManifest);
